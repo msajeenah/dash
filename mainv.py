ed49e6fe4f922aa53bfa6c98926cd35c454a5608
msajeenah <142747145+msajeenah@users.noreply.github.com>
2023-09-04 00:49:26 +0300

    Update mainv.py

--- mainv.py
+++ mainv.py
@@ -383,7 +383,62 @@ if selected == "Market Basket analysis":
 
 if selected == "Data Visulaization":
     st.markdown("## Data Visulaization")
-st.text("There is a sharp drop in inertia when moving from 2 to 3 clusters, and another noticeable decrease from 3 to 4 clusters. After that, the rate of decrease becomes more gradual. Therefore, the elbow seems to be around K=3 or K=4.
-The highest silhouette score is 0.9946 for K=2. However, this unusually high silhouette score might suggest that one cluster is much larger than the other, which might not be very informative. The next highest scores are 0.5959 for K=4 and 0.5952 for K=5, which are quite close.
 
-Considering the insights from both methods, the optimal number of clusters for the dataset appears to be 4. This choice strikes a balance between maintaining compact, well-defined clusters and ensuring that the segmentation is informative and actionable.")
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import pydeck as pdk
+import plotly.express as px
+
+DATA_FILE = 'rfm_segments.csv'
+
+st.title("Interactive Plot to Analysis Final RFM Segments")
+
+@st.cache(persist=True)
+def load_data():
+    data = pd.read_csv(DATA_FILE)
+    data = data[(data['MonetaryValue'] > 0) & (data['Recency'] <=360) & (data['Frequency'] <= 100)]
+    return data
+
+data = load_data()
+segments = data['Customer Segment'].unique()
+
+#build app filters
+column = st.sidebar.multiselect('Select Segments', segments)
+recency = st.sidebar.number_input('Smaller Than Recency', 0, 360, 360)
+frequency= st.sidebar.number_input('Smaller Than Frequency', 0, 100, 100)
+monetaryValue = st.sidebar.number_input('Smaller Than Monetary Value', 0, 100000, 100000)
+
+data = data[(data['Recency']<=recency) & (data['Frequency']<=frequency) & (data['MonetaryValue']<=monetaryValue)]
+
+#manage the multiple field filter
+if column == []:
+    data = data
+else:
+    data = data[data['Customer Segment'].isin(column)]
+
+data
+
+st.subheader('RFM Scatter Plot')
+#scatter plot
+fig_scatter = px.scatter(data, x="Recency", y="Frequency", color="Customer Segment",
+                 size='MonetaryValue', hover_data=['R_Quartile', 'F_Quartile', 'M_Quartile'])
+
+st.plotly_chart(fig_scatter)
+
+#show distribution of values
+#recency
+fig_r = px.histogram(data, x="Recency", y="CustomerID", marginal="box", # or violin, rug
+                   hover_data=data.columns, title='Recency Plot')
+st.plotly_chart(fig_r)
+
+#frequency
+fig_f = px.histogram(data, x="Frequency", y="CustomerID", marginal="box", # or violin, rug
+                   hover_data=data.columns, title='Frequency Plot')
+st.plotly_chart(fig_f)
+
+#monetary value
+fig_m = px.histogram(data, x="MonetaryValue", y="CustomerID", marginal="box", # or violin, rug
+                   hover_data=data.columns, title='Monetary Value Plot')
+st.plotly_chart(fig_m)

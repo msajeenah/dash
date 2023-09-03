181bee00cb759f9d492b62cc7a6b99728c8e2919
msajeenah <142747145+msajeenah@users.noreply.github.com>
2023-09-03 23:33:34 +0300

    Add files via upload

--- model.py
+++ model.py
@@ -1,56 +1,3 @@
-
-# RFM Model - Using Quantiles
-
-RFM is a classic Lifetime and Responsiveness segmentation model. It has been trialed and tested over the years and is a great starting point for any retailer including eCommerce companies looking to manage their customer base more proactively. 
-
-```
-Recency (R) - Time since last purchase in days
-Frequency (F) - Total number of purchases
-Monetary value (M) - Total monetary value
-```
-
-* [RFM INFO](https://en.wikipedia.org/wiki/RFM_(market_research)) - More info
-* [Quantiles](https://en.wikipedia.org/wiki/Quantile) - More info on quantiles
-
-## Built With
-
-* [Python 3.7](https://www.python.org/downloads/release/python-370/) - More information 
-
-### Libraries to install 
-
-```
-pip install -r requirements.txt
-```
-
-or pip install 
-```
-plotly
-pandas
-numpy
-scipy
-statsmodels
-matplotlib
-streamlit
-```
-
-### Data
-
-* [Find the Data here](https://www.kaggle.com/carrie1/ecommerce-data) - Download the publik dataset from here
-
-## The Model
-
-You can find the code in the model model.py file 
-
-### Review the Output of the model
-After running the model and writing the csv file. You can also use a basic streamlit app included in the repository names app.py. The app will allow you to do analyse and review RFM scores in the output data. This is useful for model optimisation. You can run the app with the following command:  
-```
-streamlit run app.py
-```
-Access the app via the localhost link.
-
-### Import the libraries and dataset
-
-```Python
 ##########################################################################################################################################
 ### RFM MODEL ###
 ##########################################################################################################################################
@@ -62,11 +9,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 data = pd.read_csv('transaction_data.csv', encoding='ISO-8859-1')
-```
 
-### Create the customer table 
-
-```Python
 ##########################################################################################################################################
 ### Create the Customer Table ###
 ##########################################################################################################################################
@@ -91,11 +34,7 @@ rfmTable['InvoiceDate'] = rfmTable.InvoiceDate.dt.date
 
 today = rfmTable.InvoiceDate.max() #use the latest date in the dataset - in the real world this will be todays system date
 rfmTable['Recency'] = (today - rfmTable['InvoiceDate']).dt.days #Days since last order
-```
 
-### Stats Tests
-
-```Python
 ##########################################################################################################################################
 ### Stats Tests ###
 ##########################################################################################################################################
@@ -105,32 +44,23 @@ rfmTable = rfmTable.rename(columns={
     'sales_value':'MonetaryValue', 'InvoiceNo':'Frequency', 'InvoiceDate':'LastOrderDate'
     }
 )
-```
-![Describe RFM Data](images/DescribeRFMdata.png)
 
-```Python
 #show distribution of values
 #recency
 fig = px.histogram(rfmTable, x="Recency", y="CustomerID", marginal="box", # or violin, rug
                    hover_data=rfmTable.columns, title='Recency Plot')
 fig.show()
-```
-![Recency Plot](images/RecencyHistogramPlot.png)
-```Python
+
 #frequency
 fig = px.histogram(rfmTable, x="Frequency", y="CustomerID", marginal="box", # or violin, rug
                    hover_data=rfmTable.columns, title='Frequency Plot')
 fig.show()
-```
-![Frequency Plot](images/FrequencyHistogramPlot.png)
-```Python
+
 #monetary value
 fig = px.histogram(rfmTable, x="MonetaryValue", y="CustomerID", marginal="box", # or violin, rug
                    hover_data=rfmTable.columns, title='Monetary Value Plot')
 fig.show()
-```
-![Monetary Value Plot](images/MonetaryValueHistogramPlot.png)
-```Python
+
 #Q-Q plot of the quantiles of x versus the quantiles/ppf of a distribution.
 # set up the plot figure
 from statsmodels.graphics.gofplots import qqplot
@@ -144,11 +74,7 @@ qqplot(rfmTable.MonetaryValue, line='r', ax=axes[1,0], label='MonetaryValue')
 
 #plot all
 plt.tight_layout()
-```
-![qqplot](images/qqplot.png)
-### Build/Define the RFM score functions
 
-```Python 
 ##########################################################################################################################################
 ### RFM Score Function ###
 ##########################################################################################################################################
@@ -163,6 +89,7 @@ quantiles = quantiles.to_dict()
 rfmSegmentation = rfmTable[['CustomerID','MonetaryValue','Frequency','Recency']]
 
 # We created to classes where high recency is bad and high frequency/ money is good
+
 # 1. Arguments (x = value, work on intervals of 90 days)
 def RClass(x):
     if x <= 60:
@@ -237,11 +164,7 @@ def CustomerSegment(x):
         return "High_Value_Sleeping"
     else:
         return "Lost"
-```
 
-### Apply the Functions
-
-```Python
 ##########################################################################################################################################
 ### CALCULATE THE RFM SCORES ###
 ##########################################################################################################################################
@@ -257,10 +180,9 @@ rfmSegmentation['RFMClass'] = rfmSegmentation.R_Quartile.map(str) \
                             + rfmSegmentation.M_Quartile.map(str)
 
 # Classify customer segments based on RFM scores
+
 rfmSegmentation['Customer Segment'] = rfmSegmentation.apply(lambda x: CustomerSegment(x), axis=1)
-```
-#### Illustrate the final segments in a scatter plot
-```Python
+
 #scatter plot to display segments
 rfm_scatter = rfmSegmentation[(rfmSegmentation['MonetaryValue'] > 0) & (rfmSegmentation['Recency'] <=360) & (rfmSegmentation['Frequency'] <= 50)]
 fig = px.scatter(rfm_scatter, x="Recency", y="Frequency", color="Customer Segment",
@@ -268,21 +190,6 @@ fig = px.scatter(rfm_scatter, x="Recency", y="Frequency", color="Customer Segmen
 fig.show()
 
 # Save the results to a csv file
-output_table = rfmSegmentation.to_csv('rfm_segments.csv')
-```
-![Segment Scatter Plot](images/rfmScatter.png)
+rfmSegmentation.to_csv('rfm_segments.csv')
 
-### Deployment options
-
-* Include the computations as a part of your ETL process (I use [KNIME](https://www.knime.com/)) - as the step before writing the final customer lifetime table to your Data Warehouse.
-* Have the model run on a monthly cycle - then you have snapshots of how customers progress through the different segments over time. It is a great way to track if you are feeding enough customers into active segments to feed your MVP (most profitable customers).
-* Trigger events based on scores or segments with your CRM or campaign management system (On-boarding, Retention and Reactivation campaigns).  
-
-## Author
-
-* **Francois van Heerden** - *Experience* - [LinkedIn Profile](https://www.linkedin.com/in/francois-van-heerden-9589825a/)
-
-## Acknowledgments
-
-* Found inspiration from multiple fellow Data Scientists in the open source community.
-* But I would like to specifically highlight this post [How to segment customers with RFM analysis](https://joaocorreia.io/blog/rfm-analysis-increase-sales-by-segmenting-your-customers.html) 
+print('RFM Calculation Completed!')
